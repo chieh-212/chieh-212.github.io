3859bb3d5bc51a5debcbcfe997275bba96553908
chieh-212 <fengnight0212@gmail.com>
2026-06-01 01:52:31 +0800

    I am sorry that I forget about local test...I committed many times and I like a joker;(

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,17 @@
-// @ts-check
-
+import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
-import { defineConfig } from 'astro/config';
 
 export default defineConfig({
   site: 'https://chieh-212.github.io',
   base: '/',
+  integrations: [mdx(), sitemap()],
+  markdown: {
+    shikiConfig: {
+      // 這樣它就不會產生那個內聯的 background-color 了
+      theme: 'github-dark',
+      langs: [],
+      wrap: true,
+    },
+  },
 });
